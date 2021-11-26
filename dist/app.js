841adca09b0454d08b3690d1c9c17d209661dc9f
Mac <mac@Macs-MacBook-Air.local>
2021-11-26 15:13:52 +0100

    Implementing frontend

--- dist/app.js
+++ dist/app.js
@@ -17,10 +17,7 @@ const app = (0, express_1.default)();
 app.set('views', path_1.default.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 app.use((0, morgan_1.default)('dev'));
-app.use((0, cors_1.default)({
-    origin: ["https://week-6-task.herokuapp.com/"],
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-}));
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
